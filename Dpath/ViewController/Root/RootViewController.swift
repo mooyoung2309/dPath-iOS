9352d296cbd9a77f55a1e51bbfaeaffc987e7d4b
송영모 <mooyoung2309@gmail.com>
2022-05-21 16:00:18 +0900

    feat: rootVC 생성

---
+++ Dpath/ViewController/Root/RootViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RootViewController.swift
+//  Dpath
+//
+//  Created by 송영모 on 2022/05/21.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(false)
+    }
+
+}
